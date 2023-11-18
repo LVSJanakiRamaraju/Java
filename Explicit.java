8a8bb50825725de2c9331cee49c30de248df5ba1
L V S Janaki Rama Raju <138346986+LVSJanakiRamaraju@users.noreply.github.com>
2023-11-18 23:59:09 +0530

    Explicit Type Conversion
    Write a  Java program to illustrate Explicit type conversion

---
+++ Explicit.java
@@ -0,0 +1,10 @@
+public class Explicit {
+    public static void main(String[] args) {
+        double doubleValue = 10.5;
+        int intValue = (int) doubleValue; 
+        
+        System.out.println("doubleValue: " + doubleValue);
+        System.out.println("intValue: " + intValue);
+    }
+}
+
