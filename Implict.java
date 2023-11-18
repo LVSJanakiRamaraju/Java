0a12280451ab8efdaa5d128e5a23ae75af066dc0
L V S Janaki Rama Raju <138346986+LVSJanakiRamaraju@users.noreply.github.com>
2023-11-18 23:54:45 +0530

    Implicit type conversion
    Write a java program to illustrate the Implicit type conversion

---
+++ Implict.java
@@ -0,0 +1,9 @@
+ class ImplicitTypeConversion {
+    public static void main(String[] args) {
+        int intValue = 10;
+        double doubleValue = intValue; // Automatic conversion from int to double
+        
+        System.out.println("intValue: " + intValue);
+        System.out.println("doubleValue: " + doubleValue);
+    }
+}
